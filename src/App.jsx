295559e06cddf2d210d11972b50e175200cb5a9d
Eraclio Vega <eracliovega@Eraclios-MBP.attlocal.net>
2023-12-18 10:04:20 -0500

    starting

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+//import components
+
+
+import './App.css'
+
+function App() {
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import {Link} from "react-router-dom"
+
+function Header(props){
+   return (
+    <header>
+        <h1 className="wp-header">WhistlePig</h1>
+    </header>
+   )
+}
